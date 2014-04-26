e3938dc6cc53b4747fc5f18b5a4aa0d69681cd49
Oswaldo <Oswaldo@Phoenix>
2014-04-26 15:27:25 -0500

    Prueba2

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Estefanny and Oswaldo!");
 		System.out.println("prueba");
+		System.out.println("prueba 2");
 	}
-
 }
